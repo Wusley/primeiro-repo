d1d0d9f620dbc71b5f6c8aef1b4767faf4687b9b
Wesley Lima <wesley@thwess.(none)>
2012-04-30 15:49:26 -0300

    primeiro commit

---
+++ control.js
@@ -0,0 +1,13 @@
+
+	var http = require('http');
+
+	var server = http.createServer(function(request,response){
+		
+		response.writeHead(200, {"Contet-Type":"text/plain"});
+		response.end("Finalmente, OLÁ MUNDO");
+		
+	});
+	
+	var port = process.env.PORT || 3000;
+	
+	server.listen(port);
